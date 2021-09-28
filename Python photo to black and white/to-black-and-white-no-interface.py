88d9f31076fe570407b15391179d47d5af67ddcb
BiancaCarneiro <bianca.ccnf@hotmail.com>
2021-09-28 12:07:37 -0300

    opencv

--- Python photo to black and white/to-black-and-white-no-interface.py
+++ Python photo to black and white/to-black-and-white-no-interface.py
@@ -1,55 +1,12 @@
-from PIL import Image
-import threading
-import sys
-import time
+import cv2
 
-# we will transform the image into gray tones by using the formula bellow:
-# new_pixel = RGB(c, c, c), where c = r*0.3 + g*0.59 + b*0.11 and old_pixel = RGB(r, g, b)
-start = time.time()
-num_threads = 8
-img_name = "pictures\Avatar the Last Airbender Backgrounds 3"
-img_ext = ".jpg"
-with Image.open(img_name + img_ext) as im:
-    px = im.load()
+image = "pic1"
+img_exr = ".jpg"
 
-width, height = im.size 
+img = cv2.imread(image+img_exr)
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-if width < num_threads:
-    num_threads = width-1
-
-class Th(threading.Thread):
-    def __init__(self, num):
-        threading.Thread.__init__(self)
-        self.num = num
-
-    def run(self):
-        j = 0
-        i = self.num #takes the thread number
-        while j < height:
-            #sys.stdout.write("Thread"+str(self.num)+" i:" + str(i) +" j:" + str(j))
-            #sys.stdout.flush()
-            #print("Thread num: " + str(self.num))
-            (r,g,b) = px[i,j]
-            c = int(r*0.3 + g*0.59 + b*0.11)
-            px[i,j] = (c,c,c)
-            i += num_threads
-            if i >= width:
-                j += 1
-                i = self.num
-        
-def main():
-    a = []
-    for k in range(num_threads):
-        a.append(Th(k))
-        a[k].start()
-
-
-main()
-lixo = True
-while threading.active_count() > 1:
-    #print(threading.active_count())
-    lixo = False
-
-print("--- %s seconds ---" % (time.time() - start))
-#im.show()
-#im.save(img_name + "_gray" + img_ext)
+cv2.imshow("Original", img)
+cv2.imshow("Black and white", img_gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
